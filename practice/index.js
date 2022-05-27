5f057bc37661236ecf978fdcf0a434ce7c11496a
gauravraj22 <105232271+gauravraj22@users.noreply.github.com>
2022-05-28 00:02:19 +0530

    Add files via upload

---
+++ practice/index.js
@@ -0,0 +1,9 @@
+function height(){
+    let first = document.getElementById('first')
+    if(second.style.height != '150px'){
+    second.style.height = '150px';
+    }
+    else {
+        second.style.height = '100px';
+    }
+}
