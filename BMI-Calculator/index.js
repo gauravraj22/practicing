7a8626f0f8228b5e2d6395b2bbcac98ac9e00e7c
gauravraj22 <105232271+gauravraj22@users.noreply.github.com>
2022-06-09 15:22:29 +0530

    Update index.js

--- BMI-Calculator/index.js
+++ BMI-Calculator/index.js
@@ -1,40 +1,16 @@
-// console.log("hii");
-
-// let h = 1.98;
-// let w = 100;
-// let bmi = w/(h*h);
-// console.log(bmi);
-
-//Selectors Variable
-var weight = parseFloat(document.getElementById("weight").value);
-let height = parseFloat(document.getElementById("height").value);
-let result = document.getElementById("result");
-
-
-//Calculate BMI
-let bmi = weight/(height*height);
-
-
-//function
 function hii(){
-    // console.log("hii");
-    result.innerHTML = bmi.toFixed(2);//here toFixed is used to get only two digit after decimal.
+    var weight = parseFloat(document.getElementById("weight").value);
+
+    var height = parseFloat(document.getElementById("height").value);
+
+    let bmi = weight/(height*height);
+
+    var result = document.getElementById("result");
+    //console.log(bmi.toFixed(2));
+
+    result.innerText = bmi.toFixed(2);
 }
 
 function reload(){
     window.location.reload()
-
-    // if(weight.innerHTML != 0){
-    //   console.log(typeof(weight))
-    //     weight.innerHTML = "";
-    // }
-
-    // if(height.innerHTML>0){
-    //     console.log("hii")
-    // }
-
-    // else if(result.innerHTML>0){
-    //    console.log("hii")
-    //    result.innerHTML = "";
-    // }
 }
